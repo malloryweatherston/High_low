167b515f486747c7138d58087bb73403661a537b
malloryweatherston <mjw0909@sbcglobal.net>
2014-05-12 10:33:15 -0500

    Created functions for random number

--- highlow.php
+++ highlow.php
@@ -1,4 +1,9 @@
 <?php
 
 //computer picks random number between 1 and 100
-$number = rand()
+$min = 1;
+$max = 100;
+int rand ( int $min , int $max );
+
+//Ask user to guess a number between 1 and 100
+fwrite(STDOUT, 'Guess a number between 1 and 100.');
