350c70384ed352850e8e447dae54348007bf105c
malloryweatherston <mjw0909@sbcglobal.net>
2014-05-12 15:06:51 -0500

    Added Arguments

--- highlow.php
+++ highlow.php
@@ -1,13 +1,18 @@
 <?php
 
 //computer picks random number between 1 and 100
-$number = rand (1 , 100); 
+if ($argc == 3) {
+	echo "Please guess a number between $argv[1] and $argv[2] .";
+
+}
+
+
+$number = rand ($argv[1], $argv[2]); 
+
 
 //count variable
 $number_of_guesses = 1;
 
-//Ask user to guess a number between 1 and 100
-fwrite(STDOUT, 'Guess a number between 1 and 100');
 
 //Get the input from the user 
 $guess = fgets(STDIN); 
@@ -26,3 +31,5 @@ while ($guess != $number) {
  $number_of_guesses++;
 }
 fwrite(STDOUT, "GOOD GUESS! You guessed correctly in $number_of_guesses guesses!\n");
+
+exit(0); 
